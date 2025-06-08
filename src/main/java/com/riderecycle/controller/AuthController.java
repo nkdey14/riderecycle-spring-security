2523961678866a1ce149a04fdc232c6ef79c4d94
nkdey14 <im.pandey264@gmail.com>
2025-06-09 00:45:35 +0530

    Developed SecurityConfig class and Updated AuthController for hashing and storing the password in DB

--- src/main/java/com/riderecycle/controller/AuthController.java
+++ src/main/java/com/riderecycle/controller/AuthController.java
@@ -4,10 +4,8 @@ import com.riderecycle.payload.UserDto;
 import com.riderecycle.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,8 +22,15 @@ public class AuthController {
     public ResponseEntity<?> registerUser(
         @RequestBody UserDto userDto
     ){
+        // hashing the password before saving it to the database
+        String hashpw = BCrypt.hashpw(userDto.getPassword(), BCrypt.gensalt(12));
+        userDto.setPassword(hashpw);
         UserDto dto = userService.createUser(userDto);
-        // Logic for user registration will go here
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to RideRecycle Application";
     }
 }
